8896cebfbcd2b9415a0094d844393b6adc527c5c
shujun.qin <shujun.qin@shanbay.com>
2017-11-21 13:19:54 +0800

    add markdown page style

--- src/views/MarkDown.js
+++ src/views/MarkDown.js
@@ -11,22 +11,66 @@ class MarkDown extends Component {
         this.handleChange = this.handleChange.bind(this);
 
         this.state = {
-            html: 'loading',
+            html: 'Please Edit...',
         };
     }
 
     componentDidMount() {
-        this.mdElement.focus();
-        this.html = 'loading...';
+        const example = [
+            '# hello, This is Markdown Live Preview',
+            '',
+            '----',
+            '## what is Markdown?',
+            'see [Wikipedia](http://en.wikipedia.org/wiki/Markdown)',
+            '',
+            '> Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people "to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)".',
+            '',
+            '----',
+            '## usage',
+            '1. Write markdown text in this textarea.',
+            "2. Click 'HTML Preview' button.",
+            '',
+            '----',
+            '## markdown quick reference',
+            '# headers',
+            '',
+            '*emphasis*',
+            '',
+            '**strong**',
+            '',
+            '* list',
+            '',
+            '>block quote',
+            '',
+            '    code (4 spaces indent)',
+            '[links](http://wikipedia.org)',
+            '',
+            '----',
+            '## changelog',
+            '* 17-Feb-2013 re-design',
+            '',
+            '----',
+            '## thanks',
+            '* [markdown-js](https://github.com/evilstreak/markdown-js)',
+            '',
+        ].join('\n');
+        this.mdElement.value = example;
+        // this.mdElement.focus();
+        this.handleChange();
 
-        this.handleChange = this.handleChange.bind(this);
+        // this.autoGrow();
     }
 
-    handleChange(...args) {
-        console.log(this.mdElement.value);
+    handleChange() {
         this.setState({
             html: converter.makeHtml(this.mdElement.value),
         });
+        this.autoGrow();
+    }
+
+    autoGrow() {
+        this.mdElement.style.height = '5px';
+        this.mdElement.style.height = this.mdElement.scrollHeight + 'px';
     }
 
     render() {
@@ -44,6 +88,7 @@ class MarkDown extends Component {
                 <div className="markdown-preview-container">
                     <div
                         id="markdown-preview"
+                        className="markdown-body"
                         dangerouslySetInnerHTML={{ __html: html }}
                     />
                 </div>
