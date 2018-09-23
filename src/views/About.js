cde4977cd4a1b4cd8d5c486df2230944f5c90c8e
jojochilly <joan_qin@126.com>
2018-09-24 00:03:33 +0800

    feat: add new blog; add track; add contact

--- src/views/About.js
+++ src/views/About.js
@@ -9,7 +9,9 @@ export default class About extends Component {
                     <p>做一个网站并不那么简单，尤其是对于拖延症晚期患者来说。</p>
                     <p>千辛万苦，终于建成雏形，可以慢慢迭代了。</p>
                     <p>第一步，准备搭一个博客。</p>
+                    <p>第二步，准备逐渐深化，支持各类常见功能。</p>
                     <p>See Ya.</p>
+                    <p>联系我：微信搜索【 joanqin_ 】，一起交流技术与管理心得。</p>
                 </div>
             </div>
         );
